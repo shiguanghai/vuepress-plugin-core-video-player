b009ed95ca10e74949e6a041d626e3e4165178e5
shiguanghai <1932275668@qq.com>
2021-01-31 14:12:54 +0800

    冲突解决

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ module.exports = (options, context) => {
   define () {
     const { lang, resolution } = options
     return {
-      lang: lang || 'zh-CN', // zh-CH/en/jp
-      resolution: resolution || '720p' // 360p/720p/1080p
+      lang: lang || 'zh-CN' // 定义播放器语言：zh-CH/en/jp
     }
   },
   name: 'vuepress-plugin-core-video-player',
   enhanceAppFiles: resolve(__dirname, './bin/enhanceAppFile.js'),
-  globalUIComponents: 'corevideoplayer'
+  //globalUIComponents: 'corevideoplayer'
 }
