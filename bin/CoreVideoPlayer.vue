ab90532f639d35944b9957a7713081ea6edbb1e4
longWay_comeOn <2538966470@qq.com>
2021-01-31 10:27:08 +0800

    属性配置基本完成

--- bin/CoreVideoPlayer.vue
+++ bin/CoreVideoPlayer.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="player-container">
     <vue-core-video-player
-      :src = src
+      :src="src"
+      :muted="muted"
+      :cover="cover"
+      :volume="volume"
+      :preload="preload"
+      :loop="loop"
+      :logo="logo"
+      :title="title"
+      :controls="controls"
     ></vue-core-video-player>
   </div>
 </template>
@@ -12,13 +20,47 @@ import Vue from "vue";
 export default {
   name: "corevideoplayer",
   props: {
-    src: String
+    src: {
+      type: [String, Array],
+      require: true,
+    },
+    muted: {
+      type: Boolean,
+      default: false, //默认不静音
+    },
+    cover: {
+      type: String,
+    },
+    volume: {
+      //无法控制
+      type: Number,
+    },
+    preload: {
+      validator: function (value) {
+        return ["none", "metadata", "metadata "].indexOf(value) !== -1;
+      },
+    },
+    loop: {
+      type: Boolean,
+    },
+    logo: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    controls: {
+      type: [Boolean, String],
+      validator: function (value) {
+        return ["fixed", "auto", false, true].indexOf(value) !== -1;
+      },
+    },
   },
   mounted() {
     Vue.use(VueCoreVideoPlayer, {
       lang: lang, // 语言
-      resolution: resolution // 清晰度
-    })
-  }
-}
+      resolution: resolution, // 清晰度
+    });
+  },
+};
 </script>
